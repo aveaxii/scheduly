9d6900709249fb52983516b7cede8bf37289c958
Kim Alex <kimalex2004dev@gmail.com>
2025-07-12 20:51:57 +0900

    Add github pages deploy flow

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/scheduly/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
